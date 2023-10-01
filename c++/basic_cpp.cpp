c974ab3699213292030e7144d70eab8d642dc8cd
Pranjal <pranjalkhatri950@gmail.com>
2023-10-01 10:59:44 +0530

    Relevant message

---
+++ c++/basic_cpp.cpp
@@ -0,0 +1,15 @@
+//#include tells the compiler to include the given file in your current file\
+//iostream means input output stream: it handles all the reading writing in your program
+#include<iostream>
+
+//namespace is nothing but a group containing different functions
+//std means standard and not sexually transmitted disease in our context
+using namespace std;
+
+//main body of our program
+int main(){
+    //cout means console output
+    cout<<"Hello World";
+    //return 0 to tell successful compilation
+    return 0;
+}
